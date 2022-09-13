a5ecb9eb538ccc257107602bd784a1004b8c21fc
Simon Mutch <smutch@unimelb.edu.au>
2022-09-13 12:14:38 +1000

    Finish first pass

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::process::Command;
 
 use clap::Parser;
+use eyre::{eyre, Result};
 use log::debug;
-use eyre::{Result, eyre};
+use tabled::{
+    object::Rows,
+    width::{Max, MinWidth},
+    Disable, Modify, Width,
+};
+use terminal_size::terminal_size;
 
 #[derive(Parser)]
 struct Cli {
     #[clap(short, long)]
-    jobid: u32
+    jobid: u32,
 }
 
 fn main() -> Result<()> {
@@ -23,17 +29,34 @@ fn main() -> Result<()> {
 
     let stdout = String::from_utf8(output.stdout)?;
 
-    // htable = horizontal table
-    let htable: Vec<_> = stdout.lines().collect();
-    let mut rows: Vec<_> = htable[0].split('|').map(|v| vec!(v)).collect();
-
-    for entry in &htable[2..] {
-        entry.split('|').zip(&mut rows).for_each(|(e, r)| r.push(e));
+    let mut table_builder = tabled::builder::Builder::default();
+    for row in stdout
+        .lines()
+        .enumerate()
+        .filter(|&(ii, _)| ii != 1)
+        .map(|(_, v)| v)
+    {
+        // TODO: There must be a better way to do this than creating a vec and popping! (peekable?)
+        let mut cols: Vec<_> = row.split('|').collect();
+        cols.pop();
+        table_builder.add_record(cols);
     }
+    let mut table_builder = table_builder.index();
+    table_builder.transpose().hide_index();
+    let table = table_builder.build();
+    let width: usize = terminal_size()
+        .ok_or(eyre!("Failed to get current terminal width"))?
+        .0
+         .0
+        .into();
+    debug!("Term width = {width}");
 
-    for row in rows {
-        println!("{}", row.join(" | "));
-    }
+    let table = table
+        .with(Modify::new(Rows::first()).with(MinWidth::new(Max)))
+        .with(Width::truncate(width))
+        .with(Disable::Row(0..1));
+
+    println!("{table}");
 
     Ok(())
 }
